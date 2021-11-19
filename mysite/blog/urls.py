3d0759609fbbf07c080bfa3ceaf0e50e62196c89
Ha Nguyen <habom2310@gmail.com>
2021-11-19 18:08:16 +1100

    new feature add blog post

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,4 +6,6 @@ app_name = 'blog'
 urlpatterns = [ 
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.detail, name='detail'),
+    path('add/', views.add, name='add'),
+    
 ]
